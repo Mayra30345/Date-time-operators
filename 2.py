3ff0cbd3b2016fa0675d1831cab2462b31fa532d
Mayra30345 <palmayra966@gmail.com>
2025-08-23 17:09:33 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,7 @@
+from datetime import date,time,datetime
+today=date.today()
+now=datetime.now()
+print("Todays date is ",today)
+print("Current time  is ",now)
+print("\n date components: ",today.year,today.month,today.day)
+
